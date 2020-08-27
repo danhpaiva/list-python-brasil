b62945fe3f5a7c82463ed7eca35166c40ab02afc
Paiva <danpaiva@live.com>
2020-08-27 20:07:55 -0300

    Exercício 2

---
+++ estruturaCondicional/exercicio02.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+numero = input("Informe um número: ")
+
+if int(numero) < 0:
+    print("O número " + numero + " é negativo")
+else : print("O número " + numero + " é positivo")
