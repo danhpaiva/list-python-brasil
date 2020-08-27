77219f901510b376099e6ff77b576890a5284b54
Paiva <danpaiva@live.com>
2020-08-27 19:41:13 -0300

    Exercício 9

---
+++ estruturaSequencial/exercicio09.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça a temperatura em graus Fahrenheit,
+# transforme e mostre a temperatura em graus Celsius.
+
+f = input("Informe a temperatura em Fahrenheit: ")
+
+c = 5 * ((float(f) - 32) / 9)
+
+print("A temperatura é de " + str(c) + " graus Celsius")
