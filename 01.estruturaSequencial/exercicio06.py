fb54ffee5e7e584bf5b42ba5a402400352f36a09
Daniel Paiva <danpaiva@live.com>
2020-09-01 18:32:37 -0300

    Exercício 06

---
+++ 01.estruturaSequencial/exercicio06.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+raio = float(input("Informe o raio do círculo: "))
+
+areaCirculo = 3.14 * (raio * raio)
+
+print("A área é de ", areaCirculo)
