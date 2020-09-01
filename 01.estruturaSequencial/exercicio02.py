e68e1d9f23cd0d42161905c7a1530d77fd1754a0
Daniel Paiva <danpaiva@live.com>
2020-09-01 18:19:08 -0300

    Exercício 02

--- 01.estruturaSequencial/exercicio02.py
+++ 01.estruturaSequencial/exercicio02.py
@@ -1,1 +1,4 @@
-# Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]. 
+# Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+numero = input("Informe um número: ")
+print("O número é :", numero)
