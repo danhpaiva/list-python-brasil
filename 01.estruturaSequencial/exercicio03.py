8aec28df11553b83f5137cf7ed87f975e5b47342
Daniel Paiva <danpaiva@live.com>
2020-08-31 23:49:02 -0300

    Exercicio 03

---
+++ 01.estruturaSequencial/exercicio03.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça dois números e imprima a soma.
+
+numero1 = int(input("Digite o 1º número: "))
+
+numero2 = int(input("Digite o 2º número: "))
+
+print("A soma de " + numero1 + " e " + numero2 + " é :" + (numero1+numero2))
