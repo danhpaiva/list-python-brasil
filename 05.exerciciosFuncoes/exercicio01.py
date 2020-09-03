f1f705c006af50d5a910ec66fc49cfef571662af
Daniel Paiva <danpaiva@live.com>
2020-09-03 19:43:29 -0300

    Exercício 01

---
+++ 05.exerciciosFuncoes/exercicio01.py
@@ -0,0 +1,13 @@
+# Faça um programa para imprimir: 1
+#    2   2
+#    3   3   3
+# n   n   n   n   n   n  ... n
+# para um n informado pelo usuário.
+# Use uma função que receba um valor n inteiro e imprima até a n-ésima linha.
+
+def receberNumero():
+    numero = input("Informe um número: ")
+    
+    print("Numero informado: ", numero)
+
+receberNumero()
