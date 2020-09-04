856ce192e7ee1c1470bf8181218889f16bc7920e
Daniel Paiva <danpaiva@live.com>
2020-09-04 00:10:30 -0300

    Exercício 01 ao 05

--- 05.exerciciosFuncoes/exercicio01.py
+++ 05.exerciciosFuncoes/exercicio01.py
@@ -1,13 +1,21 @@
-# Faça um programa para imprimir: 1
+# Faça um programa para imprimir:
+#    1
 #    2   2
 #    3   3   3
 # n   n   n   n   n   n  ... n
 # para um n informado pelo usuário.
 # Use uma função que receba um valor n inteiro e imprima até a n-ésima linha.
 
-def receberNumero():
-    numero = input("Informe um número: ")
-    
-    print("Numero informado: ", numero)
+def ImprimirSequencia(n):
+    for c in range(1, n + 1):
+        cont = 1
+        while True:
+            print(c, end=' ')
+            if cont == c:
+                break
+            cont += 1
+        print()
 
-receberNumero()
+
+n = int(input('Informe um número(limite): '))
+ImprimirSequencia(n)
---
+++ 05.exerciciosFuncoes/exercicio03.py
@@ -0,0 +1,8 @@
+# Faça um programa,
+# com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
+
+def Soma(a, b, c):
+    return a + b + c
+
+
+print(Soma(10, 5, 10))
