e958f121daf525959c0312275f44def72b682a78
Daniel Paiva <danpaiva@live.com>
2020-09-04 23:22:25 -0300

    Exercicio 03

--- 01.estruturaSequencial/exercicio03.py
+++ 01.estruturaSequencial/exercicio03.py
@@ -5,4 +5,4 @@ numero1 = int(input("Digite o 1º número: "))
 numero2 = int(input("Digite o 2º número: "))
 
 print("A soma de " + str(numero1) + " e " +
-      str(numero2) + " é : " + str(numero1+numero2))
+      str(numero2) + " é : " + str(numero1+numero2) + ".")
