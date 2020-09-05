fcd58d6cc9e4d8102f12c0d78e3ccf0a70e367e9
Daniel Paiva <danpaiva@live.com>
2020-09-05 16:44:46 -0300

    Exercicio 03 ao 05

---
+++ 06.exerciciosComStrings/exercicio03.py
@@ -0,0 +1,20 @@
+'''Nome na vertical. Faça um programa que solicite o nome do usuário e imprima-o na vertical.
+F
+U
+L
+A
+N
+O
+'''
+
+def informarNome():
+    nome = input("Informe seu nome: ")
+    return nome
+
+
+def imprimirNomeVertical():
+    nome = informarNome()
+    print("\n".join(nome))
+
+
+imprimirNomeVertical()
---
+++ 06.exerciciosComStrings/exercicio05.py
@@ -0,0 +1,33 @@
+'''Nome na vertical em escada invertida. Altere o programa anterior de modo que a escada seja invertida.
+FULANO
+FULAN
+FULA
+FUL
+FU
+F
+'''
+
+
+def informarNome():
+    nome = input("Informe seu nome: ")
+    return nome
+
+
+def imprimirNomeVertical():
+    nome = informarNome()
+    print()
+    
+    tamanho = len(nome)
+    contador = 0
+    nomeReduzido = " ".join(nome)
+    nomeReduzido = nomeReduzido.split(" ")
+
+    while tamanho > contador:
+        if contador == 0:
+            print(nome)
+        else:
+            nomeReduzido.pop()
+            print("".join(nomeReduzido))
+        contador += 1
+
+imprimirNomeVertical()
