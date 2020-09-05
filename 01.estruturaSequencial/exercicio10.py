ca1ea96b0d673f95f162f08361ac4539b21a4e66
Daniel Paiva <danpaiva@live.com>
2020-09-04 23:24:20 -0300

    Exercicio 10

--- 01.estruturaSequencial/exercicio10.py
+++ 01.estruturaSequencial/exercicio10.py
@@ -1,3 +1,6 @@
 # Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
 
-c = float(input("Informe a temperatura em Celsius"))
+c = float(input("Informe a temperatura em Celsius: "))
+f = (5 * (c - 32)) / 9
+
+print("A temperatura em graus Fahrenheit: ", f)
