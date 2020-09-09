3ec149e30d7f133b9954bfc8ae211d0c2fe3e3b8
Daniel Paiva <danpaiva@live.com>
2020-09-09 11:09:09 -0300

    Exercício 13

--- 01.estruturaSequencial/exercicio13.py
+++ 01.estruturaSequencial/exercicio13.py
@@ -5,11 +5,27 @@ utilizando as seguintes fórmulas:
     Para homens: (72.7*h) - 58
     Para mulheres: (62.1*h) - 44.7 
 '''
-print("\tCálculo do Peso Ideal\n")
 
-altura = float(input("Informe a sua altura: "))
 
-pesoIdeal = (72.7 * altura) - 58
-pesoIdeal = round(pesoIdeal, 2)
+def main():
+    print("\tCálculo do Peso Ideal\n")
+    sexo = input("Informe seu sexo: [F]Feminino | [M]Masculino: ").upper()
+    h = float(input("Informe a sua altura: "))
+    print("Seu peso ideal é de: " + str(CalcularPeso(sexo, h)) + " Kg.")
 
-print("Seu peso ideal é de: " + str(pesoIdeal) + " Kg.")
+
+def CalcularPeso(sexo, h):
+    global pesoIdeal
+    if sexo == "M":
+        pesoIdeal = (72.7 * h) - 58
+        pesoIdeal = round(pesoIdeal, 2)
+        return pesoIdeal
+    elif sexo == "F":
+        pesoIdeal = (62.1 * h) - 44.7
+        pesoIdeal = round(pesoIdeal, 2)
+        return pesoIdeal
+    else:
+        return "Sexo inválido!"
+
+
+main()
