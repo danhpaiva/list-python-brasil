1412ba4706cce251e215f133ff48e14941bea65c
Paiva <danpaiva@live.com>
2020-09-24 23:38:22 -0300

    Exercício 01

---
+++ 02.estruturaDecisao/exercicio01.py
@@ -0,0 +1,11 @@
+# Faça um Programa que peça dois números e imprima o maior deles.
+
+numeroUm = int(input("Informe o primeiro número: "))
+numeroDois = int(input("Informe o segundo número: "))
+
+if numeroUm > numeroDois:
+    print("O número " + str(numeroUm) + " é o maior.")
+elif numeroDois > numeroUm:
+    print("O número " + str(numeroDois) + " é o maior.")
+else:
+    print("Os números são iguais.")
