2a5fc4b0c91703383423e692ef1905bc8d4edf61
Paiva <danpaiva@live.com>
2020-09-26 22:41:13 -0300

    Exercício 03

---
+++ 02.estruturaDecisao/exercicio03.py
@@ -0,0 +1,11 @@
+# Faça um Programa que verifique se uma letra digitada é "F" ou "M".
+# Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+
+letra = input('Informe uma letra: [F]Feminino | [M]Masculino :').upper()
+
+if letra == 'F':
+    print('\t\nSexo Feminino')
+elif letra == 'M':
+    print('\t\nSexo Masculino')
+else:
+    print('\t\nSexo inválido')
