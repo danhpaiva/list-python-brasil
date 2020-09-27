c015b9abe0e66d61eec8568634f39585352833eb
Paiva <danpaiva@live.com>
2020-09-26 22:43:09 -0300

    Exercício 04

---
+++ 02.estruturaDecisao/exercicio04.py
@@ -0,0 +1,10 @@
+# Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
+letra = input('Digite uma letra: ')
+
+vogal = 'aeiou'
+
+if letra in vogal:
+    print('\nA letra é uma vogal!')
+else:
+    print('\nA letra é uma consoante!')
