2f301aa0076974f91bdf7c9376c4be80e69afd51
Paiva <danpaiva@live.com>
2020-09-28 11:03:39 -0300

    Exercício 06

---
+++ 02.estruturaDecisao/exercicio06.py
@@ -0,0 +1,12 @@
+# Faça um Programa que leia três números e mostre o maior deles.
+
+numeroUm = float(input('Digite o primeiro número: '))
+numeroDois = float(input('Digite o segundo número: '))
+numeroTres = float(input('Digite o terceiro número: '))
+
+if numeroUm > numeroDois and numeroUm > numeroTres:
+    print("O maior número é o : " + str(numeroUm))
+elif numeroDois > numeroUm and numeroDois > numeroTres:
+    print("O maior número é o : " + str(numeroDois))
+else:
+    print("O maior número é o : " + str(numeroTres))
