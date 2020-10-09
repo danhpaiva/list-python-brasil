a9ed60b6a1b0a27e9599ea9600838f0ff4e2ac5d
Paiva <danpaiva@live.com>
2020-10-09 20:43:55 -0300

    Exercício 13

---
+++ 03.estruturaRepeticao/exercicio13.py
@@ -0,0 +1,14 @@
+''' Faça um programa que peça dois números, base e expoente, 
+calcule e mostre o primeiro número elevado ao segundo número. 
+Não utilize a função de potência da linguagem.'''
+
+base = int(input('Informe a base: '))
+exponent = int(input('Informe o expoente: '))
+result = 1
+counter = 1
+
+while counter <= exponent:
+    result *= base
+    counter += 1
+
+print(f'\nResultado de {base} elevado a {exponent}: {result}')
