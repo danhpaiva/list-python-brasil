69cd19991a18866c1580abcc757cd0876d0ae346
Paiva <danpaiva@live.com>
2020-10-10 17:24:27 -0300

    Exercício 01

---
+++ 04.exerciciosListas/exercicio01.py
@@ -0,0 +1,8 @@
+# Faça um Programa que leia um vetor de 5 números inteiros e mostre-os
+
+list_numbers = []
+
+for number in range(5):
+    list_numbers.append(input('Informe o número: '))
+
+print(list_numbers)
