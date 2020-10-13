a04e158b59607ddf0356b0aff82bbf6d5a007edb
Paiva <danpaiva@live.com>
2020-10-12 21:12:46 -0300

    Exercício 06

--- 04.exerciciosListas/exercicio06.py
+++ 04.exerciciosListas/exercicio06.py
@@ -4,10 +4,28 @@
 # -------------
 # author: Paiva
 # -------------
-lista_nota = [[],[],[],[]]
+import os
+
+lista_nota = []
 media = []
+aluno = 1
+contador_aluno_aprovado = 0
 
 print('\nNotas dos Alunos')
+for i in range(10):
+    print()
+    soma = 0
+    for j in range(4):
+        nota = int(input(f'Notas do {aluno}º aluno: '))
+        lista_nota.append(nota)
+        soma += nota
+    aluno += 1
+    media.append(soma/4)
 
+for i in range(10):
+    if media[i] >= 7:
+        contador_aluno_aprovado += 1
 
-    
+os.system('cls')
+print(f'Médias: {media}\n')
+print(f'Existem: {contador_aluno_aprovado} alunos aprovados')
