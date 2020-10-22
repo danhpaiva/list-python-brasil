a5214c784ecccc1415cee53798d35d6166201cc9
Paiva <danpaiva@live.com>
2020-10-22 10:40:04 -0300

    update

--- 01.estruturaSequencial/exercicio13.py
+++ 01.estruturaSequencial/exercicio13.py
@@ -1,4 +1,4 @@
-'''Tendo como dado de entrada a altura (h) de uma pessoa, 
+'''Tendo como dado de entrada a altura (h) de uma pessoa,
 construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
 
     Para homens: (72.7*h) - 58
@@ -7,26 +7,16 @@ construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmu
 # author: Paiva
 # -------------
 
+print("\tCálculo do Peso Ideal\n")
+sexo = input("Informe seu sexo: [F]Feminino | [M]Masculino: ").upper()
+h = float(input("Informe a sua altura: "))
+pesoIdeal = 0
 
-def main():
-    print("\tCálculo do Peso Ideal\n")
-    sexo = input("Informe seu sexo: [F]Feminino | [M]Masculino: ").upper()
-    h = float(input("Informe a sua altura: "))
-    print("Seu peso ideal é de: " + str(CalcularPeso(sexo, h)) + " Kg.")
+if sexo == "M":
+    pesoIdeal = (72.7 * h) - 58
+    pesoIdeal = round(pesoIdeal, 2)
+elif sexo == "F":
+    pesoIdeal = (62.1 * h) - 44.7
+    pesoIdeal = round(pesoIdeal, 2)
 
-
-def CalcularPeso(sexo, h):
-    global pesoIdeal
-    if sexo == "M":
-        pesoIdeal = (72.7 * h) - 58
-        pesoIdeal = round(pesoIdeal, 2)
-        return pesoIdeal
-    elif sexo == "F":
-        pesoIdeal = (62.1 * h) - 44.7
-        pesoIdeal = round(pesoIdeal, 2)
-        return pesoIdeal
-    else:
-        return "Sexo inválido!"
-
-
-main()
+print("Seu peso ideal é de: " + str(pesoIdeal) + " Kg.")
